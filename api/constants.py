096fcafd588d52d17eac5c79e020329da3eb2259
Amine M. Boulouma <amine@boulouma.com>
2019-10-26 01:45:40 +0200

    Added 3aransia api

--- api/constants.py
+++ api/constants.py
@@ -1,6 +1,10 @@
-CONTROLLED_DICTIONARY = 'controlled_dictionary.csv'
-OPEN_DICTIONARY = 'open_dictionary.csv'
-MOROCCAN_ALPHABET = 'moroccan_alphabet.csv'
+BASE_DIR = './api'
+DATA_DIR = '/data'
+
+CONTROLLED_DICTIONARY = '/controlled_dictionary.csv'
+OPEN_DICTIONARY = '/open_dictionary.csv'
+DICTIONARY_SAMPLE = '/dictionary_sample.csv'
+MOROCCAN_ALPHABET = '/moroccan_alphabet.csv'
 
 UPPER_ENGLISH_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
 LOWER_ENGLISH_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -18,3 +22,8 @@ MOROCCAN_ENDING_LETTERS = ['d', 'a', 'o', 'w', 'r', 'z', 'u']
 
 DUPLICATE_MOROCCAN_LETTERS = ['aa', 'bb', 'cc','dd','ee','ff','gg','hh','ii','iy','jj', 
     'kk', 'll', 'mm','nn','oo','pp', 'qq', '77', '22', '55', '99', 'rr', 'ss', 'tt','uu','ww','xx', 'yi', 'yy','zz']
+
+WORD_NOT_FOUND_ERROR = { 'error_message': {
+                            'message': 'Word not found', 
+                            }
+                        }
---
+++ main.py
@@ -0,0 +1,61 @@
+import json
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from api.algorithms import *
+from api.constants import *
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+moroccan_words = pd.read_csv(BASE_DIR + DATA_DIR + DICTIONARY_SAMPLE)
+moroccan_words = moroccan_to_arabic(' '.join(moroccan_words["LDM"]))
+
+# Index
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({'message' : 'Hello, World!'})
+
+# Get all the moroccan words
+@app.route('/moroccan_words', methods=['GET'])
+def returnAll():
+    return jsonify({'moroccan_words' : moroccan_words})
+
+# Look for a word in the dictionary
+@app.route('/moroccan_words/<string:moroccan_word>', methods=['GET'])
+def returnOne(moroccan_word):
+    the_one = dict(WORD_NOT_FOUND_ERROR)
+    for i, w in enumerate(moroccan_words):
+        if w['moroccan_word'] == moroccan_word:
+            the_one = moroccan_words[i]
+    return jsonify({'moroccan_word' : the_one})
+
+# Add a word to the dictionary
+@app.route('/moroccan_words', methods=['POST'])
+def addOne():
+    new_moroccan_word = request.get_json()
+    moroccan_words.append(new_moroccan_word)
+    return jsonify({'moroccan_words' : moroccan_words})
+
+# Edit a word on the dictionary
+@app.route('/moroccan_words/<string:moroccan_word>', methods=['PUT'])
+def editOne(moroccan_word):
+    new_moroccan_word = request.get_json()
+    for i,w in enumerate(moroccan_words):
+      if w['moroccan_word'] == moroccan_word:
+        moroccan_words[i] = new_moroccan_word    
+    ws = request.get_json()
+    return jsonify({'moroccan_words' : moroccan_words})
+
+# Delete a word from the dictionary
+@app.route('/moroccan_words/<string:moroccan_word>', methods=['DELETE'])
+def deleteOne(moroccan_word):
+    for i,w in enumerate(moroccan_words):
+      if w['moroccan_word'] == moroccan_word:
+        del moroccan_words[i]  
+    return jsonify({'moroccan_words' : moroccan_words})
+
+if __name__ == "__main__":
+    app.run(debug=True)
