3efb495ac6b46ec2064969a29a39e2332201e67a
Amine M. Boulouma <amine@boulouma.com>
2019-10-28 23:48:50 +0100

    Deploying

--- api/algorithms.py
+++ api/algorithms.py
@@ -153,14 +153,7 @@ def letter_to_substitute(l):
 
 # Word counter
 def word_count(_str):
-    counts = dict()
-    words = _str.split()
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-    return counts
+    return {word: _str.count(word) for word in _str.split()}
 
 # Get duplicated
 def generate_duplicates(_str):
