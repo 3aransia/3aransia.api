e137b78be89e9e4c97e5d4432e6c6f8796a84ac4
Amine M. Boulouma <amine@boulouma.com>
2020-02-17 00:24:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def hello_world():
 # Transliteration moroccan to moroccan arabic
 @app.route('/transliterate_moroccan_route/<string:moroccan_entry>', methods=['GET'])
 def transliterate_moroccan_route(moroccan_entry):
-    return jsonify({'moroccan_transliteration': transliterate_moroccan(' '.join(moroccan_entry.split('+')))})
+    return jsonify({'moroccan_arabic_transliteration': transliterate_moroccan(' '.join(moroccan_entry.split('+')))})
 
 
 # Transliteration moroccan arabic to moroccan
 @app.route('/transliterate_moroccan_arabic_route/<string:moroccan_arabic_entry>', methods=['GET'])
 def transliterate_moroccan_arabic_route(moroccan_arabic_entry):
-    return jsonify({'moroccan_arabic_transliteration': transliterate_moroccan_arabic(' '.join(moroccan_arabic_entry.split('+')))})
+    return jsonify({'moroccan_transliteration': transliterate_moroccan_arabic(' '.join(moroccan_arabic_entry.split('+')))})
 if __name__ == "__main__":
     app.run(debug=True)
